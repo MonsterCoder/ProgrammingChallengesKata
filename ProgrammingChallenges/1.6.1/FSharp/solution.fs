7c5f10805383ffaa872a513d53829018524ffac4
Monstercoder <Monstercoder@gmail.com>
2012-03-12 22:52:16 -0500

    added FSharp implementation for 1.6.1

--- ProgrammingChallenges/1.6.1/FSharp/solution.fs
+++ ProgrammingChallenges/1.6.1/FSharp/solution.fs
@@ -2,9 +2,16 @@ printfn "start ..."
 
 let inputs = [(1, 10); (100, 200); (201, 210); (900, 1000)]
 
-let cycleLength x = x
+let rec cycleLength x length= 
+    match x with
+    | 1 -> length + 1
+    | x when x % 2 =0 -> cycleLength ((/) x 2) ((+) length 1)
+    | _ -> cycleLength ((+) ((*) x 3) 1) ((+) length 1)
 
-let printTuple (first, last) =
-    printfn "%i %i %i" first last ((+) first last)
+let cal x y = 
+    ([x..y] |> Seq.map (fun num -> cycleLength num 0)) |> Seq.max
 
-Seq.iter (printTuple) inputs
+let printResult (first, last) =
+    printfn "%i %i %i" first last (cal first last)
+
+Seq.iter (printResult) inputs
