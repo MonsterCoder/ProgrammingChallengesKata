3588d8c7e10fe06880ba8c56f82cebc654a82b44
Monstercoder <Monstercoder@gmail.com>
2012-03-12 23:00:39 -0500

    renamed some bindings for FSharp implementation of 1.6.1

--- ProgrammingChallenges/1.6.1/FSharp/solution.fs
+++ ProgrammingChallenges/1.6.1/FSharp/solution.fs
@@ -3,10 +3,11 @@ printfn "start ..."
 let inputs = [(1, 10); (100, 200); (201, 210); (900, 1000)]
 
 let rec cycleLength x length= 
+    let lgt = (+) length 1
     match x with
-    | 1 -> length + 1
-    | x when x % 2 =0 -> cycleLength ((/) x 2) ((+) length 1)
-    | _ -> cycleLength ((+) ((*) x 3) 1) ((+) length 1)
+    | 1 -> lgt
+    | x when x % 2 =0 -> cycleLength ((/) x 2) lgt
+    | _ -> cycleLength ((+) ((*) x 3) 1) lgt
 
 let cal x y = 
     ([x..y] |> Seq.map (fun num -> cycleLength num 0)) |> Seq.max
