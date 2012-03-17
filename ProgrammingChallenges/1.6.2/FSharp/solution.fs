f7f638e3e24aedb8e4f756f9398d78bd58c1ade7
Monstercoder <Monstercoder@gmail.com>
2012-03-17 00:00:42 -0500

    adding fharp solution to 1.6.2

--- ProgrammingChallenges/1.6.2/FSharp/solution.fs
+++ ProgrammingChallenges/1.6.2/FSharp/solution.fs
@@ -2,37 +2,39 @@ open System
 open System.Text.RegularExpressions
 
 let inputs = ["4 4","*...","....",".*..","....","3 5","**...",".....",".*...","0 0"]
-s
+
 let (|End|_|) (line:string) =
     let rst = Regex.Match(line, @"^0\s+0$")
     if rst.Success then
-       Some line
+       Some []
     else
        None
 
 let (|FieldHead|_|) (line:string) =
     let rst = Regex.Match(line, @"^\d\s+\d$")
     if rst.Success then
-       Some line
+       let m = Regex.Matches(line, @"\b\d+\b")
+       Some [for r in m ->r.Value]
     else
        None
 
 let (|Row|_|) (line:string) =
      let rst = Regex.Match(line, @"^[\.*]+$")
      if rst.Success then
-        Some line
+        Some [for c in line -> c]
      else
         None
 
 let Classify line =
    match line with
-   | End s-> printfn "End %s" s
-   | FieldHead s -> printfn "Head %s" s
-   | Row s -> printfn "Row %s" s
+   | End s-> printfn "End %A" s
+   | FieldHead [a;b]-> printfn "Head %A %A" a  b
+   | Row s -> printfn "Row %A" s
    | _  -> printfn "Unknown"
 
 printfn "Start..."
-Classify "4 4"
+
+Classify "3 5"
 Classify "0 0"
 Classify "*...."
 Classify "*.a..."
