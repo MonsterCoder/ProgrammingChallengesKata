348e618da7454a9afa62b5479962714cda878558
Monstercoder <Monstercoder@gmail.com>
2012-04-08 23:33:23 -0500

    1.6.2 fsharp imp.

--- ProgrammingChallenges/1.6.2/FSharp/solution2.fs
+++ ProgrammingChallenges/1.6.2/FSharp/solution2.fs
@@ -1,7 +1,7 @@
 open System
 open System.Text.RegularExpressions
 
-let inputs1 = ["3 4";"*...";"....";"....";"0 0"]
+let inputs1 = ["2 4";"1100";".*..";"0 0"]
 let inputs = ["4 4";"*...";"....";".*..";"....";"3 5";"**...";".....";".*...";"0 0"]
 
 type Line =
@@ -15,25 +15,33 @@ let parseLine line =
     else
          Mines([for c in line -> c])
 
-let rec cal chars acc = 
-   match chars with
-        | [] -> ([], acc)
-        | '*'::tail ->  let (c,r) = (acc + 1) |> cal tail 
-                        ('*'::c, r)
-        | '.'::tail ->  let (c,r) = acc |> cal tail 
-                        (r.ToString().Chars(0)::c,r)    
-        | i::tail ->    let (c,r) =  acc |> cal tail 
-                        let s = ((Int32.Parse (i.ToString())) + r).ToString().Chars(0)
-                        (s::c,acc)  
-        | _ -> ([], acc)
+let add = function
+        | (n,'*') -> '*'
+        | (n,'.') -> n.ToString().Chars(0)
+        | (n,i) -> ((Int32.Parse (i.ToString())) + n).ToString().Chars(0)
+
+let countMines = function
+        |'*' -> 1
+        | _ -> 0
+
+let rec cal = function
+        | ['*';b2;'*';f2] -> ['*';add (2,b2);'*';add (1,f2)]
+        | ['*';b2;f1;f2] ->  let f = countMines b2
+                             ['*';add (1,b2);add ((1 + f),f1);add (1,f2)]
+        | [b1;b2;'*';f2] ->  let b = List.sumBy countMines [b2;'*';f2] 
+                             [add(b,b1);add(1,b2);'*';add(0,f2)]
+        | [b1;b2;f1;f2] ->   let f = countMines b2
+                             let s = List.sumBy countMines [b2;f2] 
+                             [add(s,b1);add(0,b2);add(f,f1);add(0,f2)]
 
 let rec output = function
        | (feedIn,[]) -> feedIn
        | (feedIn_1::feedIn_2::feedIn_tail, buffer_1::buffer_2::buffer_tail) ->  
-                             let ([x1;x2;y1;y2],_) = cal [feedIn_1;feedIn_2;buffer_1;buffer_2] 0
-                             printf "%A" y1
-                             let tail_result = output(x2::feedIn_tail,y2::buffer_tail)
-                             x1::tail_result
+                             let [b1;b2;f1;f2] = cal [buffer_1;buffer_2;feedIn_1;feedIn_2]
+                             printf "%A" b1
+                             f1::(output(f2::feedIn_tail,b2::buffer_tail))
+       | ([f],[b]) -> printfn " "
+                      [f]
        | _ -> []
 
 let rec MineField inputs (buffer:char list) = 
@@ -42,11 +50,13 @@ let rec MineField inputs (buffer:char list) =
                    match parseLine(head) with
                        |Controls(0,0) -> output([for i in 0..buffer.Length -> '.'],buffer)
                                          
-                       |Controls(r,c) -> output([for i in 0..buffer.Length -> '.'],buffer)
+                       |Controls(r,c) -> 
+                                         output([for i in 0..buffer.Length -> '.'],buffer)
+                                         printfn "Field"
                                          MineField tail []    
                                                                
                        |Mines(row) ->    let _buffer = output(row@['.'],buffer)
                                          MineField tail _buffer
         | [] -> buffer
                                          
-MineField inputs1 []
+MineField inputs []
