57b16707c28cb03f0558bac0cd36b1f5d58440ad
Monstercoder <Monstercoder@gmail.com>
2012-04-09 22:31:14 -0500

    1.6.2 FSharp

--- ProgrammingChallenges/1.6.2/FSharp/solution2.fs
+++ ProgrammingChallenges/1.6.2/FSharp/solution2.fs
@@ -1,9 +1,6 @@
 open System
 open System.Text.RegularExpressions
 
-let inputs1 = ["2 4";"1100";".*..";"0 0"]
-let inputs = ["4 4";"*...";"....";".*..";"....";"3 5";"**...";".....";".*...";"0 0"]
-
 type Line =
     | Controls of int*int
     | Mines of char list
@@ -20,18 +17,18 @@ let add = function
         | (n,'.') -> n.ToString().Chars(0)
         | (n,i) -> ((Int32.Parse (i.ToString())) + n).ToString().Chars(0)
 
-let countMines = function
+let count = function
         |'*' -> 1
         | _ -> 0
 
 let rec cal = function
         | ['*';b2;'*';f2] -> ['*';add (2,b2);'*';add (1,f2)]
-        | ['*';b2;f1;f2] ->  let f = countMines b2
+        | ['*';b2;f1;f2] ->  let f = count b2
                              ['*';add (1,b2);add ((1 + f),f1);add (1,f2)]
-        | [b1;b2;'*';f2] ->  let b = List.sumBy countMines [b2;'*';f2] 
+        | [b1;b2;'*';f2] ->  let b = List.sumBy count [b2;'*';f2] 
                              [add(b,b1);add(1,b2);'*';add(0,f2)]
-        | [b1;b2;f1;f2] ->   let f = countMines b2
-                             let s = List.sumBy countMines [b2;f2] 
+        | [b1;b2;f1;f2] ->   let f = count b2
+                             let s = List.sumBy count [b2;f2] 
                              [add(s,b1);add(0,b2);add(f,f1);add(0,f2)]
 
 let rec output = function
@@ -42,21 +39,20 @@ let rec output = function
                              f1::(output(f2::feedIn_tail,b2::buffer_tail))
        | ([f],[b]) -> printfn " "
                       [f]
-       | _ -> []
+       | _ -> printfn " "
+              []
 
 let rec MineField inputs (buffer:char list) = 
     match inputs with
        | head::tail -> 
+                   let dumpRow=[for i in 0..buffer.Length -> '.']
                    match parseLine(head) with
-                       |Controls(0,0) -> output([for i in 0..buffer.Length -> '.'],buffer)
-                                         
-                       |Controls(r,c) -> 
-                                         output([for i in 0..buffer.Length -> '.'],buffer)
+                       |Controls(0,0) -> output(dumpRow,buffer) 
+                       |Controls(r,c) -> output(dumpRow,buffer)
                                          printfn "Field"
-                                         MineField tail []    
-                                                               
-                       |Mines(row) ->    let _buffer = output(row@['.'],buffer)
+                                         MineField tail []                     
+                       |Mines(mines) ->  let _buffer = output(mines@['.'],buffer)
                                          MineField tail _buffer
         | [] -> buffer
-                                         
-MineField inputs []
+
+MineField ["4 4";"*...";"....";".*..";"....";"3 5";"**...";".....";".*...";"0 0"] []
